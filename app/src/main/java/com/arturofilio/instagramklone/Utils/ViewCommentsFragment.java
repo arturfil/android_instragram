3a9c4bffc4d58d7cc015a9ffee763aad594aba9a
Arturo Filio Villa <arturfil@hotmail.com>
2018-04-17 18:55:41 -0500

    added the comment section input layout

---
+++ app/src/main/java/com/arturofilio/instagramklone/Utils/ViewCommentsFragment.java
@@ -0,0 +1,56 @@
+package com.arturofilio.instagramklone.Utils;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.arturofilio.instagramklone.R;
+import com.arturofilio.instagramklone.models.Like;
+import com.arturofilio.instagramklone.models.Photo;
+import com.arturofilio.instagramklone.models.User;
+import com.arturofilio.instagramklone.models.UserAccountSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Created by arturofiliovilla on 4/11/18.
+ */
+
+public class ViewCommentsFragment extends Fragment {
+
+    private static final String TAG = "ViewCommetsFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_comments, container, false);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/arturofilio/instagramklone/Utils/ViewPostFragment.java
+++ app/src/main/java/com/arturofilio/instagramklone/Utils/ViewPostFragment.java
@@ -152,7 +152,7 @@ public class ViewPostFragment extends Fragment {
 
                             String[] splitUsers = mUsers.toString().split(",");
 
-                            if(mUsers.toString().contains(mUserAccountSettings.getUsername())){
+                            if(mUsers.toString().contains(mUserAccountSettings.getUsername() + ",")){
                                 mLikedByCurrentUser = true;
                             } else {
                                 mLikedByCurrentUser = false;
@@ -324,6 +324,7 @@ public class ViewPostFragment extends Fragment {
         mUsername.setText(mUserAccountSettings.getUsername());
         mLikes.setText(mLikesString);
         mCaption.setText(mPhoto.getCaption());
+
 
         if(mLikedByCurrentUser) {
             mHeartWhite.setVisibility(View.GONE);
